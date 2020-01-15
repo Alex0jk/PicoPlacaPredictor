52a1c0aa8ac1aebf42fcb0cb777adda3d17ba1d8
alex0jk <alexjk6697@gmail.com>
2020-01-15 12:49:21 -0500

    Added Pico Placa controller
    added a controller that validates if input data matches with pico y placa data. 
    Unit test were made.

---
+++ PicoPlacaPredictor/PicoPlacaPredictor/Controller/PicoPlacaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PicoPlacaPredictor.ConfigData;
+
+namespace PicoPlacaPredictor.Controller
+{
+    public class PicoPlacaController
+    {
+        public bool isLastPlateDigitWithPicoPlaca(int plateLastDigit,int day)
+        {
+            if (PicoPlacaParameters.PicoPlacaDays.ContainsKey(day))
+            {
+                if (PicoPlacaParameters.PicoPlacaDays[day].Contains(plateLastDigit)){
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        public bool isTimeWithPicoPlaca(TimeSpan selectedTime)
+        {
+            TimeSpan mtime1 = PicoPlacaParameters.MorningTime1;
+            TimeSpan mtime2 = PicoPlacaParameters.MorningTime2;
+            TimeSpan atime1 = PicoPlacaParameters.AfternoonTime1;
+            TimeSpan atime2 = PicoPlacaParameters.AfternoonTime2;
+            if ((selectedTime > mtime1) && (selectedTime < mtime2) || (selectedTime > atime1) && (selectedTime < atime2))
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool isCarWithPicoPlaca(string plate,DateTime date,TimeSpan time)
+        {
+            int lastDigit = (int)plate.Last() - 48;
+            int day = (int) date.DayOfWeek;
+            Console.WriteLine(lastDigit);
+            Console.WriteLine(day);
+            if (isLastPlateDigitWithPicoPlaca(lastDigit,day) && isTimeWithPicoPlaca(time))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ PicoPlacaPredictor/PicoPlacaPredictorTests/Controller/PicoPlacaControllerPicoPlacaControllerTests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PicoPlacaPredictor.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoPlacaPredictor.Controller.Tests
+{
+    [TestClass()]
+    public class PicoPlacaControllerPicoPlacaControllerTests
+    {
+        [TestMethod()]
+        public void is_LastPlateDigit_With_PicoPlacaTest_ExpectedDay()
+        {
+            int lastDigit = 1;
+            int day = 1;
+            bool expected = true;
+
+            var controller = new PicoPlacaController();
+            bool result = controller.isLastPlateDigitWithPicoPlaca(lastDigit, day);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void is_LastPlateDigit_With_PicoPlacaTest_NotExpectedDay()
+        {
+            int lastDigit = 5;
+            int day = 5;
+            bool expected = false;
+
+            var controller = new PicoPlacaController();
+            bool result = controller.isLastPlateDigitWithPicoPlaca(lastDigit, day);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void is_LastPlateDigit_With_PicoPlacaTest_Weekend()
+        {
+            int lastDigit = 1;
+            int day = 7;
+            bool expected = false;
+
+            var controller = new PicoPlacaController();
+            bool result = controller.isLastPlateDigitWithPicoPlaca(lastDigit, day);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void is_Time_WithPicoPlacaTest_ExpectedTimeMorning()
+        {
+            TimeSpan selectedTime = new TimeSpan(8, 0, 0);
+            bool expected = true;
+
+            var controller = new PicoPlacaController();
+            bool result = controller.isTimeWithPicoPlaca(selectedTime);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void is_Time_WithPicoPlacaTest_ExpectedTimeAfternoon()
+        {
+            TimeSpan selectedTime = new TimeSpan(17, 0, 0);
+            bool expected = true;
+
+            var controller = new PicoPlacaController();
+            bool result = controller.isTimeWithPicoPlaca(selectedTime);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void is_Time_WithPicoPlacaTest_NotExpectedTime()
+        {
+            TimeSpan selectedTime = new TimeSpan(12, 0, 0);
+            bool expected = false;
+
+            var controller = new PicoPlacaController();
+            bool result = controller.isTimeWithPicoPlaca(selectedTime);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void is_Car_WithPicoPlacaTest_Expected()
+        {
+            string plate = "PQN492";
+            DateTime date = new DateTime(2020, 01, 13);
+            TimeSpan time = new TimeSpan(8, 0, 0);
+
+            bool expected = true;
+
+            var controller = new PicoPlacaController();
+            bool result = controller.isCarWithPicoPlaca(plate,date,time);
+            
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void is_Car_WithPicoPlacaTest_NotExpected()
+        {
+            string plate = "PQN2493";
+            DateTime date = new DateTime(2020, 01, 13);
+            TimeSpan time = new TimeSpan(18, 15, 0);
+
+            bool expected = false;
+
+            var controller = new PicoPlacaController();
+            bool result = controller.isCarWithPicoPlaca(plate, date, time);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
