febdeb4eba866d2623a0aae8964aabafea5dce43
alex0jk <alexjk6697@gmail.com>
2020-01-15 13:51:47 -0500

    Added input validator
    Added utility class that helps with the validation of all input variables (Plate, Date and Time).
    Regex were used and Unit test were made.

---
+++ PicoPlacaPredictor/PicoPlacaPredictor/Util/InputValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PicoPlacaPredictor.Util
+{
+    public class InputValidator
+    {
+        public string plateValidator(string plate)
+        {
+            string regex = @"^[A-Z]{3}[0-9]{3,4}$";
+            Match match = Regex.Match(plate, regex);
+            if (match.Success)
+                return plate;
+            else
+                throw new FormatException("Plate doesnt match with the specified format (PPP#### or PPP###)");
+        }
+        public DateTime dateValidator(string date)
+        {
+            string regex = @"^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$";
+            Match match = Regex.Match(date, regex);
+            if (match.Success)
+                return Convert.ToDateTime(date);
+            else
+                throw new FormatException("Date doesnt match with the specified format (yyyy-mm-dd)");
+            
+        }
+        public TimeSpan timeValidator(string time)
+        {
+            string regex = @"^([0-9]{2}:[0-9]{2})$";
+            Match match = Regex.Match(time, regex);
+            if (match.Success)
+                return Convert.ToDateTime(time).TimeOfDay;
+            else
+                throw new FormatException("Time doesnt match with the specified format (hh:mm)");
+        }
+    }
+}
---
+++ PicoPlacaPredictor/PicoPlacaPredictorTests/Util/InputValidatorValidationTest.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PicoPlacaPredictor.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoPlacaPredictor.Util.Tests
+{
+    [TestClass()]
+    public class InputValidatorValidationTest
+    {
+        [TestMethod()]
+        public void plateCar_ValidationTest_Expected()
+        {
+            string plateInput = "PQN0492";
+            string expected = "PQN0492";
+            string result;
+            InputValidator val = new InputValidator();
+            try
+            {
+                result = val.plateValidator(plateInput);
+                Assert.AreEqual(expected, result);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+            
+            
+        }
+
+        [TestMethod()]
+        public void plate_ValidationTest_NotExpected()
+        {
+            string plateInput = "PQ1N0492";
+            string expected = "Plate doesnt match with the specified format (PPP#### or PPP###)";
+            string result;
+            InputValidator val = new InputValidator();
+            try
+            {
+                result = val.plateValidator(plateInput);
+                Assert.Fail();
+            }
+            catch (FormatException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+        
+        [TestMethod()]
+        public void date_ValidationTest_Expected()
+        {
+            string dateInput = "2020-02-29";
+            DateTime expected = new DateTime(2020,2,29);
+            DateTime result;
+            InputValidator val = new InputValidator();
+            try
+            {
+                result = val.dateValidator(dateInput);
+                Assert.AreEqual(expected, result);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        [TestMethod()]
+        public void date_ValidationTest_NotExpected()
+        {
+            string dateInput = "2020/02-29";
+            string expected = "Date doesnt match with the specified format (yyyy-mm-dd)";
+            DateTime result;
+            InputValidator val = new InputValidator();
+            try
+            {
+                result = val.dateValidator(dateInput);
+                Assert.Fail();
+            }
+            catch (FormatException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void time_ValidationTest_Expected()
+        {
+            string timeInput = "07:29";
+            TimeSpan expected = new TimeSpan(7,29,0);
+            TimeSpan result;
+            InputValidator val = new InputValidator();
+            try
+            {
+                result = val.timeValidator(timeInput);
+                Assert.AreEqual(expected, result);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void time_ValidationTest_NotExpected()
+        {
+            string timeInput = "07:29:0";
+            string expected = "Time doesnt match with the specified format (hh:mm)";
+            TimeSpan result;
+            InputValidator val = new InputValidator();
+            try
+            {
+                result = val.timeValidator(timeInput);
+                Assert.Fail();
+            }
+            catch (FormatException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+    }
+}
